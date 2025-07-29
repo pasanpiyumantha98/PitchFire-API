e60980827ad3135aee40954a9d656b63633c3bcc
pasanpiyumantha98 <139437240+pasanpiyumantha98@users.noreply.github.com>
2025-07-29 11:37:24 +0530

    l1

--- src/main/java/com/example/picthfire/PicthFireApplication.java
+++ src/main/java/com/example/picthfire/PicthFireApplication.java
@@ -1,7 +1,9 @@
 package com.example.picthfire;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class PicthFireApplication {
@@ -10,6 +12,9 @@ public class PicthFireApplication {
         SpringApplication.run(PicthFireApplication.class, args);
     }
 
+    @Bean
+    ModelMapper modelMapper() {
+        return new ModelMapper();
+    }
 
-    
 }
