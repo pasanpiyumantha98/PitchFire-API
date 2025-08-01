5beb3e396db6dc167e89588bde343a495e42f158
pasanpiyumantha98 <139437240+pasanpiyumantha98@users.noreply.github.com>
2025-08-01 07:56:43 +0530

    o1

---
+++ src/main/java/com/example/picthfire/repo/PropertyRepo.java
@@ -0,0 +1,14 @@
+package com.example.picthfire.repo;
+
+import com.example.picthfire.model.Property;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+public interface PropertyRepo extends JpaRepository<Property, Integer> {
+
+    @Query(value="select max(id) from property", nativeQuery = true)
+    public Integer getMaxIdProp();
+
+
+
+}
---
+++ src/main/java/com/example/picthfire/services/PropertyService.java
@@ -0,0 +1,48 @@
+package com.example.picthfire.services;
+
+
+import com.example.picthfire.dto.PropDto;
+import com.example.picthfire.model.Property;
+import com.example.picthfire.repo.PropertyRepo;
+import com.example.picthfire.repo.UserRepo;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+
+@Service
+@Transactional
+public class PropertyService {
+
+    @Autowired
+    private ModelMapper modelMapper;
+
+    @Autowired
+    private PropertyRepo propertyRepo;
+    @Autowired
+    private UserRepo userRepo;
+
+
+    public String regprop(PropDto propDto) {
+
+    Integer maxid = propertyRepo.getMaxIdProp();
+
+    if (maxid == null) {
+        propDto.setId(1);
+
+    } else
+    {
+     propDto.setId(maxid + 1);
+    }
+
+    userRepo.save(modelMapper.map(propDto, Property.class));
+
+    return "success";
+
+
+    }
+
+
+
+}
